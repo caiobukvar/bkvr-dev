3a35c41e455eb2187fc00f9c4ffdb6311991e1cb
Caio <bukvar2009@gmail.com>
2024-02-15 10:33:08 -0300

    feat: navbar hover and current page

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,11 +1,35 @@
-import Link from "next/link";
+"use client";
 
-export default function Navbar() {
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar(req: any) {
+  const path = usePathname();
+
   return (
-    <div className="flex w-full align-center p-6 gap-6 bg-slate-600">
-      <Link href="/">Home</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/social">Social</Link>
+    <div className="flex w-full align-center p-6 gap-6 bg-slate-800">
+      <Link
+        href="/"
+        className={`hover:text-lime-400 ${path === "/" ? "text-lime-500" : ""}`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/projects"
+        className={`hover:text-lime-400 ${
+          path === "/social" ? "text-lime-500" : ""
+        }`}
+      >
+        Projects
+      </Link>
+      <Link
+        href="/social"
+        className={`hover:text-lime-400 ${
+          path === "/social" ? "text-lime-500" : ""
+        }`}
+      >
+        Social
+      </Link>
     </div>
   );
 }
