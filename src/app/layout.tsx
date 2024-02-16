60e3d4a7616b2bfc34faebebd5a81a57a86769ec
Caio <bukvar2009@gmail.com>
2024-02-16 18:23:32 -0300

    remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
       <body className={unbounded.className}>
         <Navbar />
         {children}
-        <Analytics />
         <SpeedInsights />
       </body>
     </html>
