7815402b3b86f78ec55c07478f45fb10a05721d4
Caio <bukvar2009@gmail.com>
2024-02-16 18:09:01 -0300

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
 import { inter, unbounded, jura } from "@/fonts";
 import Navbar from "./components/Navbar";
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
