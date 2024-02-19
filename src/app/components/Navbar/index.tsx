7224b9f2eec9463b087522f61986a29789713029
Caio <bukvar2009@gmail.com>
2024-02-19 17:57:56 -0300

    feat: navbar with icons

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Blocks, Briefcase, Contact, Rocket } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,38 +8,43 @@ export default function Navbar(req: any) {
   const path = usePathname();
 
   return (
-    <div className="flex w-full align-center justify-center md:justify-start lg:justify-start p-5 gap-2 text-sm md:gap-5 md:text-md bg-slate-800 sticky top-0 z-10">
+    <div className="flex w-full align-center justify-center md:justify-start lg:justify-start p-5 gap-5 text-sm bg-slate-800 sticky top-0 z-10">
       <Link
         href="/"
-        className={`hover:text-lime-400 outline-none rounded-sm p-2 focus:text-lime-800 ${
+        className={`flex gap-10 md:gap-2 items-center hover:text-lime-400 hover:underline hover:decoration-lime-600 outline-none rounded-sm p-2 focus:text-lime-800 ${
           path === "/" ? "text-lime-500" : ""
         }`}
       >
-        Home
+        <Contact />
+        <p className="hidden md:block">About me</p>
       </Link>
       <Link
         href="#experience"
-        className={`hover:text-lime-400 outline-none rounded-sm p-2 focus:text-lime-800 ${
+        className={`flex gap-10 md:gap-2 items-center hover:text-lime-400 hover:underline hover:decoration-lime-600 outline-none rounded-sm p-2 focus:text-lime-800 ${
           path === "/experience" ? "text-lime-500" : ""
         }`}
       >
-        Experience
+        <Briefcase />
+        <p className="hidden md:block">Experience</p>
       </Link>
       <Link
         href="#projects"
-        className={`hover:text-lime-400 outline-none rounded-sm p-2 focus:text-lime-800 ${
+        className={`flex gap-10 md:gap-2 items-center hover:text-lime-400 hover:underline hover:decoration-lime-600 outline-none rounded-sm p-2 focus:text-lime-800 ${
           path === "/projects" ? "text-lime-500" : ""
         }`}
       >
-        Projects
+        {" "}
+        <Blocks />
+        <p className="hidden md:block">My projects</p>
       </Link>
       <Link
         href="#social"
-        className={`hover:text-lime-400 outline-none rounded-sm p-2 focus:text-lime-800 ${
+        className={`flex gap-10 md:gap-2 items-center hover:text-lime-400 hover:underline hover:decoration-lime-600 outline-none rounded-sm p-2 focus:text-lime-800 ${
           path === "/social" ? "text-lime-500" : ""
         }`}
       >
-        Social
+        <Rocket />
+        <p className="hidden md:block">Social</p>
       </Link>
     </div>
   );
