b4b879d4a6a21f2cdb860c48b3e3c0b44e0898c9
Caio <bukvar2009@gmail.com>
2024-02-23 10:16:06 -0300

    test thumb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,14 @@ import "./globals.css";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "BKVR Development",
-  description: "Software Developer",
+  title: "<BKVR />",
+  description: "Software Developer Portfolio",
+  keywords: ["NextJS", "React", "TailwindCSS", "Javascript"],
+  robots: {
+    googleBot: {
+      index: true,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +25,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="min-h-screen bg-slate-900 text-slate-400">
+      <meta property="og:description" content="Software Developer Portfolio" />
+      <meta
+        property="og:image"
+        content="/public/images/projects/thumb-bkvr.png"
+      />
+      <meta property="og:url" content="https://bkvr.dev.br" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="<BKVR />" />
+
       <Toaster
         richColors
         toastOptions={{
