eda0ac9ab37567ce7584bfe3b70617a6185a0320
Caio <bukvar2009@gmail.com>
2024-02-23 12:16:40 -0300

    new text for about section

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -27,9 +27,9 @@ export default function About() {
             Hi! I'm 30 years old, from Brazil, delivering solutions since 2021.
           </p>
           <p>
-            Started my career in 2021, fell in love with programming when I
-            started a fullstack development course. That was my first glance on
-            the tech area, besides gaming, making me hungry for knowledge.
+            Fell in love with programming when I started a fullstack development
+            course. That was my first glance on the tech area, besides gaming,
+            making me hungry for knowledge.
           </p>
           <p>
             Nowadays, I'm commited to improve my skills as a developer, while
