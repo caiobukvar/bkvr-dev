933262fb2d692ef9a8ee7b86921d30e2be606159
Caio <bukvar2009@gmail.com>
2024-02-23 10:35:19 -0300

    feat: thumb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "sonner";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "<BKVR />",
@@ -25,6 +26,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="min-h-screen bg-slate-900 text-slate-400">
+      <Head>
+        <title>
+          {"<"}BKVR {"/>"}
+        </title>
+        <meta name="description" content="Software Developer Portfolio" />
+        <meta property="og:url" content="https://www.bkvr.dev.br" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="<BKVR />" />
+        <meta
+          property="og:description"
+          content="Software Developer Portfolio"
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/9ee1429b-345f-4bdc-82ed-2f0a3ff3ac31.png?token=03Sk1W-up-b-zlFQnd-agWkAFHSHhrCAXdobSRLfQhs&height=601&width=1200&expires=33244695074"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="bkvr.dev.br" />
+        <meta property="twitter:url" content="https://www.bkvr.dev.br" />
+        <meta name="twitter:title" content="<BKVR />" />
+        <meta
+          name="twitter:description"
+          content="Software Developer Portfolio"
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/9ee1429b-345f-4bdc-82ed-2f0a3ff3ac31.png?token=03Sk1W-up-b-zlFQnd-agWkAFHSHhrCAXdobSRLfQhs&height=601&width=1200&expires=33244695074"
+        />
+      </Head>
       <Toaster
         richColors
         toastOptions={{
