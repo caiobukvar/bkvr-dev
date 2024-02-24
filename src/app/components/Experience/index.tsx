ebbe0f12891de2c0364e5d8543c9777363a37a37
Caio <bukvar2009@gmail.com>
2024-02-24 10:34:30 -0300

    feat: experience section translation

--- src/app/components/Experience/index.tsx
+++ src/app/components/Experience/index.tsx
@@ -5,62 +5,55 @@ import {
   ChevronRight,
   GitCommitHorizontal,
 } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 /* eslint-disable react/no-unescaped-entities */
 export default function Experience() {
+  const t = useTranslations("Experience");
+  const job2ListKeys = ["i1", "i2", "i3", "i4", "i5"] as const;
+  const job1ListKeys = ["i1", "i2"] as const;
+
   return (
     <div className="mt-24 flex w-full max-w-screen-lg flex-col gap-6 p-4">
       <div className="flex items-center gap-2">
         <Briefcase />
-        <h3 className="text-xl font-bold md:text-2xl">
-          Professional experience
-        </h3>
+        <h3 className="text-xl font-bold md:text-2xl">{t("title")}</h3>
       </div>
       <div className="flex flex-col gap-12">
         <div className="flex flex-col gap-4 border-l-2 border-lime-600 pl-6 text-sm md:text-base">
           <div className="flex flex-col gap-2">
             <h2 className="font-bold text-slate-600">
-              Frontend Developer @ Unmaze
+              {t("frontend-dev")} @ Unmaze
             </h2>
 
-            <p className="text-xs">Mar. 2022 - Aug. 2023</p>
+            <p className="text-xs">
+              {t("march")} 2022 - {t("august")} 2023
+            </p>
           </div>
 
-          <p>
-            I was responsible for developing unmaze's platform and maintaining
-            it, focused on React. I've contributed with several crucial
-            insights, which lead us to:
-          </p>
+          <p>{t("job2.description")}</p>
           <ul className="ml-6 list-disc text-wrap">
-            <li>Revenue increased 40%; </li>
-            <li>Total user amount increased by 60%;</li>
-            <li>Number of active users increased by 50%;</li>
-            <li>B2B clients raised from 0 to 8 (last trimester 2022);</li>
-            <li>NPS kept above 85 throughout 2022.</li>
+            {job2ListKeys.map((key) => (
+              <li key={key}>{t(`job2.list.${key}`)}</li>
+            ))}
           </ul>
         </div>
 
         <div className="flex flex-col gap-4 border-l-2 border-lime-600 pl-6 text-sm md:text-base">
           <div className="flex flex-col gap-2">
             <h2 className="font-bold text-slate-600">
-              Frontend Developer @ Cubos Academy
+              {t("frontend-dev")} @ Cubos Academy
             </h2>
 
-            <p className="text-xs">Nov. 2021 - Mar. 2022</p>
+            <p className="text-xs">
+              {t("november")} 2021 - {t("march")} 2022
+            </p>
           </div>
-          <p>
-            I was responsible for creating layouts based on figma designs,
-            focused on React.
-          </p>
+          <p>{t("job1.description")}</p>
           <ul className="ml-6 list-disc text-wrap">
-            <li>
-              major increase of my coding skills along with a senior dev
-              reviewing my code;
-            </li>
-            <li>
-              major increase on communication skills and introduction to
-              dailies, weeklys, reports, 1 on 1's;
-            </li>
+            {job1ListKeys.map((key) => (
+              <li key={key}>{t(`job1.list.${key}`)}</li>
+            ))}
           </ul>
         </div>
       </div>
