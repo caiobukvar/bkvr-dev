2611724987db282bf0178584f8ec4a710b084e7f
Caio <bukvar2009@gmail.com>
2024-02-29 10:02:20 -0300

    feat: fixing header text +italic

--- src/app/components/TypedHeader/index.jsx
+++ src/app/components/TypedHeader/index.jsx
@@ -35,7 +35,7 @@ function TypedHeader({ headerTransaltions }) {
 
   return (
     <div className="flex w-full max-w-screen-lg">
-      <h2 className="text-3xl text-slate-600">
+      <h2 className="text-3xl italic text-slate-600">
         <span className="title1 "></span>
         <span className="dev-name text-lime-600 underline"></span>
         <span className="title2 "></span>
