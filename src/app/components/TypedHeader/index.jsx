e3b9182bad6db7b71b7eca868dfe5fffc5bb28b7
Caio <bukvar2009@gmail.com>
2024-03-01 14:59:40 -0300

    hotfix: responsiveness for mobile small, header

--- src/app/components/TypedHeader/index.jsx
+++ src/app/components/TypedHeader/index.jsx
@@ -35,7 +35,7 @@ function TypedHeader({ headerTransaltions }) {
 
   return (
     <div className="flex  w-full max-w-screen-lg">
-      <h2 className="h-4 text-3xl italic text-slate-600">
+      <h2 className="min-h-4 text-3xl italic text-slate-600">
         <span className="title1 "></span>
         <span className="dev-name font-bold text-lime-600 underline"></span>
         <span className="title2 "></span>
