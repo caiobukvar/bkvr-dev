17bf1f1048ecbf092b6ac953af311747cc1b6bb5
Caio <bukvar2009@gmail.com>
2024-11-12 09:15:18 -0300

    new icon for Achievements section

--- src/app/components/Achievments/index.tsx
+++ src/app/components/Achievments/index.tsx
@@ -1,11 +1,11 @@
-import { Award } from "lucide-react";
+import { Trophy } from "lucide-react";
 
 export default function Achievements() {
   return (
     <div className="flex w-full max-w-screen-lg flex-col gap-16 px-12">
       <div className="flex flex-col gap-6 ">
         <div className="flex items-center gap-2 text-slate-800 dark:text-slate-400">
-          <Award />
+          <Trophy />
           <h3 className="text-xl font-bold md:text-2xl">Conquistas</h3>
         </div>
         <div className="flex flex-col gap-4 border-l-2 border-lime-600 pl-6 text-sm text-slate-800 md:text-base dark:text-slate-400">
