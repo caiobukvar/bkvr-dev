e4923438623aacb18a23b77d8b1bb35db9901187
Caio Bukvar <bukvar2009@gmail.com>
2025-09-10 13:08:36 -0300

    update: about me section text update, added hobbies

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -16,15 +16,22 @@ export default function About() {
     <div className="flex w-full max-w-screen-lg flex-col gap-16 px-12 ">
       <TypedHeader headerTransaltions={headerTransaltions} />
 
-      <div className="flex flex-col gap-6 ">
-        <div className="flex items-center gap-2 text-slate-800 dark:text-slate-400">
-          <Contact />
-          <h3 className="text-xl font-bold md:text-2xl">{t("about-me")}</h3>
-        </div>
-        <div className="flex flex-col gap-4 border-l-2 border-lime-600 pl-6 text-sm text-slate-800 md:text-base dark:text-slate-400">
-          <p>{t("about-p1")}</p>
-          <p>{t("about-p2")}</p>
-          <p>{t("about-p3")}</p>
+      <div id="about-me-main" className="flex flex-col gap-6 ">
+        <div className="flex flex-col gap-2 text-slate-800 dark:text-slate-400">
+          <div id="about-me-header" className="flex gap-2">
+            <Contact />
+            <h3 className="text-xl font-bold md:text-2xl">{t("about-me")}</h3>
+          </div>
+          <div
+            id="about-me-list"
+            className="flex flex-col gap-4 border-l-2 border-lime-600 pl-6 text-sm text-slate-800 md:text-base dark:text-slate-400"
+          >
+            <p>{t("about-p1")}</p>
+            <p>{t("about-p2")}</p>
+            <p>{t("about-p3")}</p>
+            <p>{t("about-p4")}</p>
+            <p>{t("hobbies-p1")}</p>
+          </div>
         </div>
       </div>
     </div>
